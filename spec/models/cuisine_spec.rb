0823d1b52f76c10ff7df3aae3d1622969fb5ccb8
Raghu Betina <raghu@firstdraft.com>
2019-03-09 22:15:18 +0000

    Validate uniqueness of cuisines.name

--- spec/models/cuisine_spec.rb
+++ spec/models/cuisine_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Cuisine, type: :model do
 
     describe "Validations" do
 
+    it { should validate_uniqueness_of(:name) }
+
     it { should validate_presence_of(:name) }
       
     end
