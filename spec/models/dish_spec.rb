fb79aab2fb3a5f6cbc9961c0fd7a3aef8b6f0b79
Raghu Betina <raghu@firstdraft.com>
2019-03-09 22:15:18 +0000

    Validate uniqueness of dishes.name

--- spec/models/dish_spec.rb
+++ spec/models/dish_spec.rb
@@ -12,6 +12,8 @@ RSpec.describe Dish, type: :model do
 
     describe "Validations" do
 
+    it { should validate_uniqueness_of(:name) }
+
     it { should validate_presence_of(:name) }
       
     end
